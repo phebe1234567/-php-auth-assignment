d7622306452da7670b85a1dd5c6ec5cf53cd2476
iamphebeay <phebeobafunso@gmal.com>
2026-05-12 10:08:23 +0100

    first commit

--- index.php
+++ index.php
@@ -3,13 +3,27 @@ require_once 'config.php';
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Home</title>
 </head>
 <body>
-    <h1>Welcome to this page</h1>
+
+<h1>Welcome to My PHP Auth System</h1>
+
+<?php if (isset($_SESSION['user_id'])): ?>
+
+    <p>You are logged in.</p>
+
+    <a href="dashboard.php">Go to Dashboard</a><br>
+    <a href="logout.php">Logout</a>
+
+<?php else: ?>
+
+    <a href="login.php">Login</a><br>
+    <a href="register.php">Register</a>
+
+<?php endif; ?>
+
 </body>
 </html>
